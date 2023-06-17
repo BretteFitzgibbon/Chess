4279634b0366b2b72273ea06ec677f2f50d6698b
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2023-06-16 18:44:55 -0700

    Create Knight.h

---
+++ Knight.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Piece.h"
+
+class Knight : public Piece{ // Knight derives (inherits from) from Piece
+    
+  public: 
+    Knight(int _row, int _col, bool _isWhite, std::string _name); // construct a Knight at this location, of the specified color, with the specified screen name (h1b, h2b, h1w, h2w)
+    ~Knight(); // destructor
+    bool isLegalMoveTo(int _row, int _col); // true if this Knight can move to the specified location from its current location
+};
